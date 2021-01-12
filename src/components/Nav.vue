21a7c37a99558cfaaed19d240de2fbe9dcbb6353
Lisa Collignon <68648800+LisaCollignon@users.noreply.github.com>
2021-01-12 11:53:47 +0100

    remove click-away - for test navbar mobile

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,7 +9,7 @@
         <img class="h-6" src="/src/assets/_Collisa_wit.svg" alt="">
       </div>
 
-      <button @click="isActive = !isActive" v-click-away="onClickAway" class="lg:hidden rounded focus:outline-none focus:ring-2 focus:ring-purplepink-400 focus:ring-opacity-40">
+      <button @click="isActive = !isActive"  class="lg:hidden rounded focus:outline-none focus:ring-2 focus:ring-purplepink-400 focus:ring-opacity-40">
         <svg v-show="!isActive" class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
         </svg>
